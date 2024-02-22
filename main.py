88e72b8d95a4d0c66dc6a221042fa604bae0efc6
Houjun Liu <houjun@stanford.edu>
2024-02-22 10:49:11 -0800

    commiting basic prompt

--- main.py
+++ main.py
@@ -3,25 +3,30 @@ from transformers import AutoTokenizer, LlamaForCausalLM
 import torch
 import torch.nn.functional as F
 
-model = LlamaForCausalLM.from_pretrained("/juice5/scr5/nlp/llama-2-hf-latest/Llama-2-7b-chat-hf", load_in_4bit=True, use_cache=True, low_cpu_mem_usage=True)
+model = LlamaForCausalLM.from_pretrained("/juice5/scr5/nlp/llama-2-hf-latest/Llama-2-7b-chat-hf", use_cache=True, low_cpu_mem_usage=True, device_map = 'cuda')
 tokenizer = AutoTokenizer.from_pretrained("/juice5/scr5/nlp/llama-2-hf-latest/Llama-2-7b-chat-hf")
 
 # grab ids for "sure", "likely", and "impossible"
-TOK_SURE = tokenizer.encode("definitely")[-1]
+TOK_SURE = tokenizer.encode("sure")[-1]
 TOK_LIKELY = tokenizer.encode("possible")[-1]
 TOK_IMPOSSIBLE = tokenizer.encode("impossible")[-1]
 
-TOK_ONE = tokenizer.encode("Q")[-1]
-TOK_TWO = tokenizer.encode("P")[-1]
+TOK_ONE = tokenizer.encode("C")[-1]
+TOK_TWO = tokenizer.encode("K")[-1]
 
 
 while True:
     # premise = input("(premise) > ").strip()
     # statement = input("(statement) > ").strip()
     # judgement = input("(sure/likely/impossible) > ").strip()
-    premise = input("(premise) > ").strip()
-    hypothesis1 = input("(hypothesis1) > ").strip()
-    hypothesis2 = input("(hypothesis2) > ").strip()
+    task = input("(task) > ").strip()
+    step = "-"
+    steps = []
+    while step != "":
+        step = input("(step) > ").strip()
+        if step != "":
+            steps.append(step)
+    steps = "\n".join(steps)
 
     # premise = "I'm very athletic."
     # statement = "I'm very athletic."
@@ -29,28 +34,28 @@ while True:
     # hypothesis = "I'm going to climb to the top of sparkle montain."
     
     prompt = f"""
-Judge which of the two statements logically make sense next to each other. 
-
-premise: "I'm very smart"
-response A: "I just failed my math test"
-response B: "I just aced my math test"
-
-Judge:
-response B, because a smart person will ace their math test.
-
-premise: "I love humanities"
-response S: "I am an author"
-response K: "I am a scientist"
-
-Judge:
-response S, because a love of humanities means you will write a lot.
-
-premise: "{premise}"
-response Q: "{hypothesis1}"
-response P: "{hypothesis2}"
-
-Judge:
-response """
+Use numbers and basic arithmetic operations (+ - * /) to obtain 24. Given an input and an answer, give a judgement (sure/impossible) if the answer is correct, i.e. it uses each input exactly once and no other numbers, and reach 24.
+Input: 4 4 6 8
+Answer: (4 + 8) * (6 - 4) = 24
+Judge: sure
+Input: 2 9 10 12
+Answer: 2 * 12 * (10 - 9) = 24
+Judge: sure
+Input: 4 9 10 13
+Answer: (13 - 9) * (10 - 4) = 24
+Judge: sure
+Input: 4 4 6 8
+Answer: (4 + 8) * (6 - 4) + 1 = 25
+Judge: impossible
+Input: 2 9 10 12
+Answer: 2 * (12 - 10) = 24
+Judge: impossible
+Input: 4 9 10 13
+Answer: (13 - 4) * (10 - 9) = 24
+Judge: impossible
+Input: {task}
+Answer: {steps}
+Judge: """
 
     # sample output distribution
     inputs = tokenizer(prompt.strip(), return_tensors="pt")
@@ -66,14 +71,15 @@ response """
 
     # grab the distributions for each class
     probs = F.softmax(torch.tensor([
-        distr[TOK_ONE], distr[TOK_TWO]
+        distr[TOK_SURE], distr[TOK_IMPOSSIBLE]
         ]), dim=0)
 
-    print("> ", ["one", "two"][torch.argmax(probs).item()])
-    print("(desired) > ", tokenizer.decode(torch.argmax(distr).item()))
+    print("(prompting) > ", ["one", "two"][torch.argmax(probs).item()])
+    # print("(desired) > ", tokenizer.decode(torch.argmax(distr).item()))
     print(probs)
 
-    # res = tokenizer.batch_decode(generate_ids.cpu(), skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    # res = tokenizer.batch_decode(model.generate(inputs.input_ids.cuda(), max_new_tokens=50, do_sample=False, temperature=0))[0] 
+            # skip_special_tokens=true, clean_up_tokenization_spaces=false)[0][len(prompt)-1:]
     # print(res)
     breakpoint()
 
